6f327734de598e23b7d7e4a19515d1f02fdeb5a5
tmdev <tmdev82@gmail.com>
2015-12-08 15:43:02 +0100

    added new classes

---
+++ KanjiListCreator/Data/KanjiListRow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanjiListCreator.Data
+{
+    public class KanjiListRow
+    {
+        public int MaxColumnCount { get; set; }
+
+        private List<ScrapedKanji> _myColumns;
+
+        public bool AddKanjiToRow(ScrapedKanji kanji)
+        {
+            bool result = true;
+
+            //check if kanji is already in the list
+            if(_myColumns == null)
+            {
+                _myColumns = new List<ScrapedKanji>();
+            }
+
+            //columns may not exceed the maximum number
+            if(MaxColumnCount == _myColumns.Count)
+            {
+                return false;
+            }
+
+            foreach(ScrapedKanji listKanji in _myColumns)
+            {
+                if(listKanji.Word.Equals(kanji.Word,StringComparison.InvariantCultureIgnoreCase))
+                {                    
+                    result = false;
+                    break;
+                }
+            }
+
+            if(result)
+            {
+                _myColumns.Add(kanji);
+            }
+            
+            return result;
+        }
+
+        public IEnumerable<ScrapedKanji> GetColumns()
+        {
+            return _myColumns;
+        }
+
+    }
+}
---
+++ KanjiListCreator/Data/KanjiSortedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanjiListCreator.Data
+{
+    public class KanjiSortedList
+    {
+        public int MaxRows { get; set; }
+
+        private List<KanjiListRow> _myRows;
+        public List<KanjiListRow> MyContent
+        {
+            get{ return _myRows; } 
+        }
+
+        public KanjiSortedList()
+        {
+            _myRows = new List<KanjiListRow>();
+        }
+
+
+        
+    }
+}
